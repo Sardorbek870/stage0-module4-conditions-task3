a0da4fe1cf279639be29a06c4443fb0762a5aaf1
Stas Sukora <st.sukora@gmail.com>
2022-11-21 22:18:50 +0300

    Create README.mdsardor1

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(!Character.toString(character).matches("[a-z][A-Z]"))
+            System.out.println("wrong alphabet!");
+        else if(Character.toString(character).matches("[aieouAIEOU]"))
+            System.out.println("Vowel");
+        else
+            System.out.println("Consonant");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
+        String string = "[a-z][A-Z]";
+
+        if(Character.toString(symbol).matches(string))
+            System.out.println("English");
+        else
+            System.out.println("Non English");
     }
 }
